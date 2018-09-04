66c1dac636bc30eed76f0d1824dc4d8d340d06f2
nickreit@icloud.com <nickreit@icloud.com>
2018-09-04 19:31:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
   array.unshift(element)
-  return array
+  return array.unshift(element)
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
